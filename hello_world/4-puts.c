095b178c4372cc83d9fa0006626b39006300c4d8
Jose Goldsmith <13070@holbertonstudents.edu>
2026-02-10 02:20:03 +0000

    updated 4-puts file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
 * my_function - does my stuff
 * @my_arg: its mine damnit
 *
 * Does my stuff explained.
-*/ 
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
