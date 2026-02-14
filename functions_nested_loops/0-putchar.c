293bd4a8f872bf4ae76251a274cb9290dc621220
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-02-14 00:33:58 +0000

    .

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints
+ * out the phrase
+ * _putchar
+ */
+
+int main (void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
