7c98f417ab0b765912f52504f4008e192c7efcac
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-02-16 03:09:50 +0000

    created function file to multiple two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 numbers together
+ *
+ * @a: is the first number.
+ * @b: is the second number.
+ *
+ * Return: result of numbers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
