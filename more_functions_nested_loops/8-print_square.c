c1b6abf9d19f9563a3c6c95a95f49480a5732b1d
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-02-17 03:52:28 +0000

    added 8th file

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - prints out a square
+ *
+ * @size: is the value to enter to make its size.
+ */
+
+void print_square(int size)
+{
+	int width, height;
+
+	for (height = 0; height < size; height++)
+	{
+		for (width = 0; width < size; width++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
