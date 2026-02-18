48326e0c94d67af05ff16ff282853010567baa31
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-02-18 01:07:07 +0000

    added 1st file

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+/**
+ * swap_int - swaps two integers values
+ *
+ * @a: is the pointer to first value
+ *
+ * @b: is pointer to second value
+ *
+ */
+
+ void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b; 
+	*b = temp;
+}
