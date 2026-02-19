d2ba44944b7c323cd92e2b88741bb704fdd8f24f
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-02-19 12:46:22 +0000

    added 5th file

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses the actual string value
+ *
+ * @s: string value to enter
+ *
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int left = 0;
+	int temp, right;
+
+	while (s[len] != '\0')
+	{
+		len++;
+		
+	}
+
+	right = len -1;
+
+	while (left < right)
+	{
+		temp = s[left];
+		s[left] = s[right];
+		s[right] = temp;
+		left++;
+		right--;
+	}
+}
