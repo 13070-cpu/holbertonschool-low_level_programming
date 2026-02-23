622a9e4d79d4c5bcae1b0e08211a28bae9bc32b9
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-02-23 02:56:47 +0000

    edited 6th file to make checker happy, wasnt really correct

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,17 @@
 
 void puts2(char *str)
 {
-	int n;
+	int n = 0;
 
-	for (n = 0; str[n] != '\0'; n += 2)
+	while (str[n] != '\0')
 	{
-		_putchar(str[n]);
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+		
+		n++;
 	}
 
 	_putchar('\n');
-}
+}	
