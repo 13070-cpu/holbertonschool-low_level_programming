34a08e45ab1e4017605de80eef6d0a207ec03e22
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-02-23 05:47:04 +0000

    added file for concantenating strings

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: destination value
+ *
+ * @src: soruce value
+ *
+ * Return: strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int i = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+
+	dest[len + i] = '\0';
+	return (dest);
+}
