6ef551302536257c5fa8d3ae86a4f9cfbce93e99
Jose Goldsmith <13070-cpu@holbertonstudents.edu>
2026-03-03 00:25:38 +0000

    made betty happy

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints string in reverse
- * @s: string input
- * 
- * Return: 0.
+ * _print_rev_recursion - prints a string in reverse
+ * @s: the string to print
+ *
+ * Return: void
  */
-
 void _print_rev_recursion(char *s)
 {
+	/* 1. Base Case: If we hit the end, stop diving */
 	if (*s == '\0')
 	{
 		return;
 	}
 
+	/* 2. The Dive: Call the function for the next character first */
 	_print_rev_recursion(s + 1);
+
+	/* 3. The Action: Print character only after the next calls finish */
 	_putchar(*s);
 }
